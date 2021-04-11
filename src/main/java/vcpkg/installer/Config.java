82fd8ce29f6379709decdd48a0899765c6c80aa3
Eugene Kravchenko <Zzzheka97@gmail.com>
2021-04-11 23:44:56 +0300

    Update Config.java

--- src/main/java/vcpkg/installer/Config.java
+++ src/main/java/vcpkg/installer/Config.java
@@ -3,5 +3,5 @@ package vcpkg.installer;
 import java.nio.file.Path;
 
 public interface Config {
-    String VCPKG_PATH = Path.of("/home/zzzheka/tmpd/vcpkg/vcpkg").toString();
+    String VCPKG_PATH = "vcpkg";
 }
